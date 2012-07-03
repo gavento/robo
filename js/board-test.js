a1e3b58aae76385572f535019cb4321d0577074f
Tomas Gavenciak <gavento@ucw.cz>
2012-07-03 22:56:23 +0200

    Moving cards<!

--- js/board-test.js
+++ js/board-test.js
@@ -13,8 +13,8 @@ function showBoard(board) {
 }
 
 function activateBoard() {
-    log('<span class="showHistory" onmouseover="showBoard(gameHistory['+turn+'])" onmouseout="showBoard(b)">' +
-        'Turn '+turn+' (shows gameHistory)');
+    $('#log').append('<div class="showHistory" onmouseover="showBoard(gameHistory['+turn+'])" onmouseout="showBoard(b)">' +
+        'Turn '+turn+' (shows gameHistory)</div>');
     gameHistory.push(b.clone());
     turn++;
     b.activateBoard();
@@ -62,5 +62,76 @@ $(document).ready(function() {
     var t = $('#board0')[0];
     b.resetTable(t);
     b.drawTable(t);
+
+    var c = [];
+    c.push(new SimpleCard('S R', 106, 'SR'));
+    c.push(new SimpleCard('3xS', 301, 'SSS'));
+    c.push(new SimpleCard('2xS', 202, 'SS'));
+    c.push(new SimpleCard('L 2xS', 208, 'LSS'));
+    c.push(new SimpleCard('U', 42, 'U'));
+    c.push(new SimpleCard('R', 18, 'R'));
+    c.push(new SimpleCard('L', 43, 'L'));
+    c.sort(function(a,b){return b.priority - a.priority});
+    _(c).each(function(card) {
+	$('.card-pool').append(card.e);
+	$(card.e).draggable({revert: "invalid"});
+	});
+    $('.card-plan-slot').each(function($e) {
+	$(this).droppable({ drop: function(event, ui) { planCard(ui.draggable, $(this)); } });
+    });
+    $('.card-pool').disableSelection();
+    $('.card-pool').droppable({ drop: function(event, ui) { unplanCard(ui.draggable); } });
 });
+
+function unplanCard($card) {
+  $card.appendTo(".card-pool");
+  $card.css('left', 0);
+  $card.css('top', 0);
+}
+
+function planCard($card, $slot) {
+  var $planned = $slot.children('.card-face');
+  if ($planned.length > 0)
+    unplanCard($planned)
+  $card.appendTo($slot);
+  $card.css('left', 0);
+  $card.css('top', 0);
+}
+
+function Card(text, priority) {
+    this.text = text;
+    this.priority = priority;
+    this.e = $('<div class="card-face"><i>' + priority + '</i><br>' + text + '</div>');
+    this.execute = function(robot) {
+	log("Playing card " + this.text + " on " + robot);
+    }
+}
+
+/* Commands are given as a character sequence: 
+ * S - step
+ * J - jump (not really implemented)
+ * L - turn left
+ * R - turn right
+ * U - U-turn
+ */
+function SimpleCard(text, priority, commands) {
+    goog.base(this, text, priority);
+    this.commands = commands;
+    this.execute = function(robot) {
+	goog.base(this, 'execute', robot);
+	_.each(this.commands, function(c) {
+	    log("Executing command " + c + " on " + robot);
+	    cmds = { S: robot.step,
+		     J: robot.jump,
+		     L: robot.turnLeft,
+		     R: robot.turnRight,
+		     U: robot.uTurn };
+	    if (cmds[s])
+		cmds[c].call(robot);
+	    else
+		throw ("Invalid command " + c);
+	});
+    }
+}
+goog.inherits(SimpleCard, Card);
 
